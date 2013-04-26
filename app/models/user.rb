b5cd1bb42202a1f40d5903e84a3786565252467c
Apprentice <apprentice@dbc04.local>
2013-04-26 16:20:14 -0700

    competed user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,38 @@
 class User < ActiveRecord::Base
   has_many :rounds
-  # Remember to create a migration!
+  
+  validates :email, :uniqueness => true
+  validates :name, :email, :password, :presence => true
+
+  validate :valid_email
+  validate :min_password_length
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def valid_email
+    unless email =~ /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+      errors.add(:email, "Invalid email")
+    end
+  end
+
+  def min_password_length
+    unless password.length > 6
+      errors.add(:password, "Password too short, must be at least six characters")
+    end
+  end
+
+
+
+
 end
+
+
