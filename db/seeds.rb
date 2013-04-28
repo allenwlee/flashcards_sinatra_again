d4e0c272e225769683cc35fdb1432f9001e2a8d8
DBC Apprentice <apprentice@devbootcamp.com>
2013-04-28 12:06:04 -0700

    database seeded decks and cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,23 @@
-User.create(name: "mike", email: "m@ike.com", password_hash: "123")
+
+
+User.create(name: "mike", email: "m@ike.com", password: "viewsonic")
+
+
+@movies_deck = Deck.create(category: 'movies')
+@state_capitals_deck = Deck.create(category: 'state_capitals')
+@music_deck = Deck.create(category: 'music')
+
+CSV.foreach('oscar_winners.csv') do |row|
+   @movies_deck.cards << Card.create(question: row[0], answer: row[1])
+end
+
+CSV.foreach('artists_songs.csv') do |row|
+  @music_deck.cards << Card.create(question: row[0], answer: row[1])
+end
+
+CSV.foreach('state_capitals.csv') do |row|
+  @state_capitals_deck.cards << Card.create(question: row[0], answer: row[1])
+end
+
+
+
