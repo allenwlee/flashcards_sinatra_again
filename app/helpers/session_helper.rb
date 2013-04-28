de91fee1441616f1676d613454a2b6e4d9964fcf
Apprentice <apprentice@devbootcamp.com>
2013-04-28 14:42:39 -0700

    round controller implementation

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -7,4 +7,8 @@ helpers do
   def current_user
     @current_user = User.find(session[:id]) if session[:id]
   end
+
+  def current_round
+     @current_round = Round.find(session[:round]) if session[:round]
+  end
 end
